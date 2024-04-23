c82a79cfcd89c383f2e73d5f21cf707afeabcfa3
Rahul Sharma <rahul190201@gmail.com>
2024-04-23 12:26:09 +0530

    deplyoment changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import Chart from 'chart.js/auto'
+import Chart from './chart.js/auto'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
